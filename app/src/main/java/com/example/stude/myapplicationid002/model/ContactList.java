0a36373ad16b3a3ab970481b87273501936fde85
knyazbalkonsky <studentknyazev@gmail.com>
2017-02-20 15:33:21 +0400

    Up!

---
+++ app/src/main/java/com/example/stude/myapplicationid002/model/ContactList.java
@@ -0,0 +1,16 @@
+package com.example.stude.myapplicationid002.model;
+
+import java.util.TreeSet;
+
+/**
+ * Created on 20.02.2017.
+ * At 15:22.
+ * Today is понедельник
+ */
+public class ContactList {
+    private TreeSet<Contact> contacts;
+
+    ContactList(){
+        contacts = new TreeSet<>();
+    }
+}
